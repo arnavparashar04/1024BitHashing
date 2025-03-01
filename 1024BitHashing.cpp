dbdd88a2df5002d677b056a4e3eebf43f44e6816
arnavparashar04 <arnavparashar408@gmail.com>
2025-03-01 22:20:46 +0530

    Fixed for better usage

--- 1024BitHashing.cpp
+++ 1024BitHashing.cpp
@@ -15,10 +15,10 @@ uint64_t Salt(const string &password, const string &username)
     uint64_t salt = PRIME1;
     for (char c : (password + username))
     {
-        salt ^= static_cast<uint64_t>(c) * PRIME2;                
-        salt = (salt << 7) | (salt >> (64 - 7));           //shifting
+        salt ^= static_cast<uint64_t>(c) * PRIME2;
+        salt = (salt << 7) | (salt >> (64 - 7)); // shifting
         salt *= PRIME1;
-        salt ^= (salt >> 17) | (salt << (64 - 17));  // more mixing
+        salt ^= (salt >> 17) | (salt << (64 - 17)); // more mixing
     }
     return salt;
 }
@@ -28,7 +28,7 @@ vector<uint64_t> Lattice(const string &password, uint64_t salt)
     vector<uint64_t> poly(HASH_LENGTH / 64);
     for (size_t i = 0; i < password.size(); i++)
     {
-        poly[i % poly.size()] ^= (static_cast<uint64_t>(password[i]) + salt) * PRIME1; 
+        poly[i % poly.size()] ^= (static_cast<uint64_t>(password[i]) + salt) * PRIME1;
     }
     return poly;
 }
@@ -40,25 +40,25 @@ void memoryHardening(vector<uint64_t> &poly, uint64_t salt)
     {
         memory[i] = (poly[i % poly.size()] + salt) ^ PRIME2;
         memory[i] = (memory[i] << 13) | (memory[i] >> (64 - 13));
-        
     }
-    for (size_t round = 0; round < 5; round++) { 
-        for (size_t i = 0; i < poly.size(); i++) {
+    for (size_t round = 0; round < 5; round++)
+    {
+        for (size_t i = 0; i < poly.size(); i++)
+        {
             poly[i] ^= memory[i % memory.size()];
             poly[i] *= PRIME1;
             poly[i] ^= (poly[i] << 19) | (poly[i] >> (64 - 19));
         }
     }
-    
 }
 
 void bitwiseMixing(vector<uint64_t> &poly)
 {
     for (size_t i = 0; i < poly.size(); i++)
     {
-        poly[i] ^= (poly[i] << 31) | (poly[i] >> (64 - 31) );
+        poly[i] ^= (poly[i] << 31) | (poly[i] >> (64 - 31));
         poly[i] *= PRIME1;
-        poly[i] ^= (poly[i] << 17) | (poly[i] >> (64 - 17) );
+        poly[i] ^= (poly[i] << 17) | (poly[i] >> (64 - 17));
     }
 }
 
@@ -68,7 +68,7 @@ string generateHash(const vector<uint64_t> &poly)
     hashStream.fill('0');
     for (size_t i = 0; i < poly.size(); i++)
     {
-        hashStream << hex << setw(16) << poly[i]; //setw to ignore after 16, 0s if less
+        hashStream << hex << setw(16) << poly[i]; // setw to ignore after 16, 0s if less
     }
     return hashStream.str();
 }
@@ -84,14 +84,17 @@ string Hash(const string &password, const string &username)
 
 int main()
 {
-    string username, password;
-    cout << "Enter username: ";
-    cin >> username;
-    cout << "Enter password: ";
-    cin >> password;
+    char buffer = 't';
+    while (buffer != 'q')
+    {
+        string username, password;
+        cout << "Enter username: ";
+        cin >> username;
+        cout << "Enter password: ";
+        cin >> password;
+        string hash = Hash(password, username);
+        cout << hash << endl;
 
-    string hash = Hash(password, username);
-    cout << hash << endl;
-
-    return 0;
+        cin >> buffer;
+    }
 }
